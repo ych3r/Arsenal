82e6608afc96ef63033cfe1d8282e98dadaffabb
ych3r <liuyc26@gmail.com>
2023-11-06 23:23:08 -0500

    refactoring

--- MAC_Changer/mac_changer.py
+++ MAC_Changer/mac_changer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import subprocess
 import optparse
+import re
 
 def get_arguments():
     parser = optparse.OptionParser()
@@ -19,5 +20,24 @@ def change_mac(interface, mac_address):
     subprocess.call(["ifconfig", interface, "hw", "ether", mac_address])
     subprocess.call(["ifconfig", interface, "up"])
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface]).decode()
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+
 options = get_arguments()
+
+current_mac = get_current_mac(options.interface)
+print(f"Current MAC = {str(current_mac)}")
+
 change_mac(options.interface, options.mac_address)
+
+current_mac = get_current_mac(options.interface)
+if current_mac == options.mac_address:
+    print(f"[+] MAC address was successfully changed to {current_mac}")
+else:
+    print("[-] MAC address did not get changed.")
