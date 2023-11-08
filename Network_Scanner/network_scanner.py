cad90daff4503090b0452e16c3a6cdadddcfce8c
ych3r <liuyc26@gmail.com>
2023-11-08 15:13:52 -0500

    arp scanner tool

--- Network_Scanner/network_scanner.py
+++ Network_Scanner/network_scanner.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+import optparse
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="target", help="Target range to scan")
+    (options, arguments) = parser.parse_args()
+    if not options.target:
+        parser.error("[-] Please specify a target, use --help for more info.")
+    return options
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    # scapy.ls(scapy.ARP())
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    # scapy.ls(scapy.Ether())
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-scan("10.0.2.1/24")
+    clients_list = []
+    for element in answered_list:
+        clients_list.append({"ip": element[1].psrc, "mac": element[1].hwsrc})
+    return clients_list
+
+def print_result(results_list):
+    print(f"IP\t\t\tMAC Address\n------------------------------------------------------")
+    for client in results_list:
+        print(f"{client['ip']}\t\t{client['mac']}")
+
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
